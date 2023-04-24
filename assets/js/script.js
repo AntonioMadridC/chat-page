c58a3b9debba20ac7cc70d052c3e0ab006863c97
AntonioMadridC <antoniomadridc@gmail.com>
2023-04-24 18:33:04 -0400

    first commit, basic

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("btnChat").addEventListener('click', function (params) {
+    document.getElementById("chatUsers").classList.toggle("d-none")
+})
+
+//add class animación : window.innerwidth
+
+
+document.getElementById("chat2").addEventListener('click', function (params) {
+    document.getElementById("chat2").classList.remove("fw-bold")
+})
